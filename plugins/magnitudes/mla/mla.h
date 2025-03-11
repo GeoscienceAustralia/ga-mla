603bebc0180c91fc70e1f264a1f93fe686e1f31a
Anthony Carapetis <anthony.carapetis@ga.gov.au>
2025-03-11 15:19:15 +1100

    Update minSNR logic to use base class's members
    The base MagnitudeProcessor class now implements the same minSNR check,
    with members _minimumSNR and _treatAsValidMagnitude taking the place of
    our _minSNR and _validValue. Thus it no longer makes sense to define
    these ourselves - better to reuse the existing ones. Also means we no
    longer need to override setup or treatAsValidMagnitude.

--- plugins/magnitudes/mla/mla.h
+++ plugins/magnitudes/mla/mla.h
@@ -121,8 +121,6 @@ class Magnitude_MLA : public Seiscomp::Processing::MagnitudeProcessor
         // Constructor. Return a new Magnitude_MLA object.
         explicit Magnitude_MLA(const std::string& type = GA_ML_AUS_MAG_TYPE);
 
-        bool setup(const Seiscomp::Processing::Settings &settings);
-
         void setDefaults() override; 
 
         // Sets the amplitude type that is being used in the calculation.
@@ -174,7 +172,6 @@ class Magnitude_MLA : public Seiscomp::Processing::MagnitudeProcessor
         @returns The distance (R component) of the calculation formula.
         */
         static double distance(double delta, double depth);
-        bool treatAsValidMagnitude() const override;
 
     private:
 
@@ -183,8 +180,6 @@ class Magnitude_MLA : public Seiscomp::Processing::MagnitudeProcessor
         #####################################################################*/
 
         static std::map<std::string, MagCalc> regionToCalcMap;
-        bool   _validValue;
-        double _minSNR;
 
         /*#####################################################################
                                                 PRIVATE METHODS
