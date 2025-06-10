f4b32dc24f228dc9fa1ad0ba0acc3f1619384c90
Anthony Carapetis <anthony.carapetis@ga.gov.au>
2025-06-10 14:39:09 +1000

    Add script to truncate geojson coordinates
    6 decimal places is ~10cm resolution. Saves a lot of filesize.

---
+++ truncate_geojson_decimals.py
@@ -0,0 +1,18 @@
+import sys
+import json
+
+
+def trunc(x):
+    if isinstance(x, list):
+        return [trunc(y) for y in x]
+    if isinstance(x, float):
+        return round(x, 6)
+    return x
+
+
+data = json.load(sys.stdin)
+for feature in data["features"]:
+    feature["geometry"]["coordinates"] = [
+        trunc(x) for x in feature["geometry"]["coordinates"]
+    ]
+json.dump(data, sys.stdout)
